3181542d2f3dae18b7d449044cd9d2f1ab301f0c
Eshiwani Michael <michaelndolo17142@gmail.com>
2026-01-07 22:20:55 +0300

    navigation bar

--- mike-portfolio/src/components/NavBar.jsx
+++ mike-portfolio/src/components/NavBar.jsx
@@ -1,8 +1,14 @@
 function NavBar(){
     return(
-        <div>
-            navigation bar
-        </div>
+        <nav className="navBar">
+            <div className="logo">inkognito</div>
+            <ul className="nav-link">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/service">Service</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
     );
 }
 
